4104d1b7feccb5e90f52cd328bcee30307a1b73c
0xlvnk <kalvinaditya205@gmail.com>
2025-06-17 23:43:01 +0700

    Update list.sh

--- menu/ssh/list.sh
+++ menu/ssh/list.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 clear
-echo -e "====== List Akun SSH ======"
-printf "%-15s %-10s %-12s %-10s\n" "Username" "Password" "Expired" "Limit"
-echo "--------------------------------------------"
+echo -e "\033[1;36m======= DAFTAR AKUN SSH =======\033[0m"
+printf "%-15s %-15s\n" "Username" "Expired Date"
+echo "-------------------------------"
 
 count=0
 
 if [[ -f /etc/ssh-db/users.db ]]; then
     while IFS='|' read -r user pass exp owner limit; do
-        printf "%-15s %-10s %-12s %-10s\n" "$user" "$pass" "$exp" "$limit"
+        printf "%-15s %-15s\n" "$user" "$exp"
         ((count++))
     done < /etc/ssh-db/users.db
 
-    echo "--------------------------------------------"
+    echo "-------------------------------"
     echo -e "Total Akun SSH: $count"
 else
     echo "Belum ada akun yang dibuat."
