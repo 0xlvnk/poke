9825c9b838f2ab0e7f84632e61ced12df012729e
0xlvnk <kalvinaditya205@gmail.com>
2025-06-17 23:52:43 +0700

    Update list.sh

--- menu/ssh/list.sh
+++ menu/ssh/list.sh
@@ -5,12 +5,14 @@ printf "%-15s %-15s\n" "Username" "Expired Date"
 echo "-------------------------------"
 
 count=0
+db_file="/etc/ssh-db/users.db"
 
-if [[ -f /etc/ssh-db/users.db ]]; then
+if [[ -f $db_file ]]; then
     while IFS='|' read -r user pass exp owner limit; do
+        [[ -z "$user" || -z "$exp" ]] && continue
         printf "%-15s %-15s\n" "$user" "$exp"
         ((count++))
-    done < /etc/ssh-db/users.db
+    done < "$db_file"
 
     echo "-------------------------------"
     echo -e "Total Akun SSH: $count"
