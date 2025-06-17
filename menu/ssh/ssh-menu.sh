b5c07c091fb76dd12d1f44b546aad717deb2e423
0xlvnk <kalvinaditya205@gmail.com>
2025-06-17 23:51:13 +0700

    Update ssh-menu.sh

--- menu/ssh/ssh-menu.sh
+++ menu/ssh/ssh-menu.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 ROOT=$(dirname "$(readlink -f "$0")")/..
-source "$ROOT/utils/menu-header.sh"
 
 while true; do
     clear
-    print_header "SSH MENU"
-    echo -e "[1]  Create SSH Account"
-    echo -e "[2]  List SSH Member"
-    echo -e "[3]  Delete SSH Account"
-    echo -e "[4]  Lock SSH User"
-    echo -e "[5]  Unlock SSH User"
-    echo -e "[6]  Extend SSH Expiry"
-    echo -e "[7]  Check Active SSH & IP Login"
-    echo -e "[8]  Change SSH Password"
-    echo -e "[x]  Back"
-    echo -ne "\\nSelect option: "; read opt
+    echo -e "\033[1;33m========== SSH MENU ==========\033[0m"
+    echo -e "1)  Create SSH Account"
+    echo -e "2)  List SSH Member"
+    echo -e "3)  Delete SSH Account"
+    echo -e "4)  Lock SSH User"
+    echo -e "5)  Unlock SSH User"
+    echo -e "6)  Extend SSH Expiry"
+    echo -e "7)  Check Active SSH & IP Login"
+    echo -e "8)  Change SSH Password"
+    echo -e "x)  Back"
+    echo -ne "\nSelect option: "; read opt
 
     case $opt in
-        1) clear; bash "$ROOT/ssh/create.sh"; read -n1 -r -p "Press any key to return..." ;;
-        2) clear; bash "$ROOT/ssh/list.sh"; read -n1 -r -p "Press any key to return..." ;;
+        1)
+            clear
+            bash "$ROOT/ssh/create.sh"
+            echo -ne "\n\033[1;33mKembali ke menu utama? [Tekan ENTER]\033[0m"
+            read
+            ;;
+        2)
+            clear
+            bash "$ROOT/ssh/list.sh"
+            echo -ne "\n\033[1;33mKembali ke menu utama? [Tekan ENTER]\033[0m"
+            read
+            ;;
         3) bash "$ROOT/ssh/delete.sh" ;;
         4) bash "$ROOT/ssh/lock.sh" ;;
         5) bash "$ROOT/ssh/unlock.sh" ;;
